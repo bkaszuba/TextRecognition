53f19348a90458fa95d74e2c40190a10a8ecc250
kaszuba <bartek.kaszuba4444@wp.pl>
2018-03-18 23:07:59 +0100

    Add 2 metrics: Chebyshev+Manhattan

---
+++ src/main/java/TextRecognition/Classification/CountVectorizer.java
@@ -0,0 +1,67 @@
+package TextRecognition.Classification;
+
+import TextRecognition.Model.Article;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class CountVectorizer {
+    private HashMap compareA;
+    private HashMap compareB;
+    private int[][] wordsCounter;
+
+    public CountVectorizer(Article a) {
+        this.compareA = extractArticleBody(a);
+    }
+
+    public void vectorizeArticle(Article b) {
+        this.compareB = extractArticleBody(b);
+    }
+
+    private HashMap extractArticleBody(Article a) {
+        HashMap<String, Integer> wordsDictionary = new HashMap<>();
+
+        String[] words = a.getBody().split("\\s+");
+        for (int i = 0; i < words.length; i++) {
+            if (wordsDictionary.containsKey(words[i])) {
+                wordsDictionary.put(words[i], wordsDictionary.get(words[i]) + 1);
+            } else
+                wordsDictionary.put(words[i], 1);
+        }
+        return wordsDictionary;
+    }
+
+    public void countWords() {
+        Set set = compareA.entrySet();
+        Iterator iterator = set.iterator();
+        int iter = 0;
+
+        this.wordsCounter = new int[compareA.size()+compareB.size()][2];
+        while (iterator.hasNext()) {
+            Map.Entry mentry = (Map.Entry) iterator.next();
+            if (compareB.containsKey(mentry.getKey())) {
+                wordsCounter[iter][1] = (int) compareB.get(mentry.getKey());
+                compareB.remove(mentry.getKey());
+            } else {
+                wordsCounter[iter][1] = 0;
+            }
+            wordsCounter[iter][0] = (int) mentry.getValue();
+            iter++;
+            //compareB.remove(mentry.getKey());
+        }
+        set = compareB.entrySet();
+        iterator = set.iterator();
+        while (iterator.hasNext()) {
+            Map.Entry mentry = (Map.Entry) iterator.next();
+            wordsCounter[iter][0] = 0;
+            wordsCounter[iter][1] = (int) mentry.getValue();
+            iter++;
+        }
+    }
+
+    public int[][] getWordsCounter() {
+        return wordsCounter;
+    }
+}
--- src/main/java/TextRecognition/Classification/Knn.java
+++ src/main/java/TextRecognition/Classification/Knn.java
@@ -10,11 +10,11 @@ import java.util.*;
  * Created by Kaszuba on 10.03.2018.
  */
 public class Knn {
-    public String [] places;
+    public String[] places;
     private List<Article> testingArticles;
-    private List<Article>  classificationArticles;
+    private List<Article> classificationArticles;
     private HashMap<Double, String> results;
-    private HashMap<String, int []> percentage;
+    private HashMap<String, int[]> percentage;
 
     public Knn(List<Article> testingValues, List<Article> classificationValues) {
         this.testingArticles = testingValues;
@@ -24,12 +24,12 @@ public class Knn {
         initializePercentage();
     }
 
-    public void classify(int k){
+    public void classify(int k) {
         double all = 0;
 
-        for (Article classifyArt: classificationArticles) {
+        for (Article classifyArt : classificationArticles) {
             all++;
-            if(all%250==0)
+            if (all % 250 == 0)
                 System.out.println(".");
             results = new HashMap<>();
             CountVectorizer countVectorizer = new CountVectorizer(classifyArt);
@@ -38,21 +38,17 @@ public class Knn {
 
                 countVectorizer.vectorizeArticle(testArt);
                 countVectorizer.countWords();
-                euclideanMetric(countVectorizer.getWordsCounter());
-                results.put(euclideanMetric(countVectorizer.getWordsCounter()), testArt.getLabel());
+                results.put(chebyshevMetric(countVectorizer.getWordsCounter()), testArt.getLabel());
 
             }
 
             Map<Double, String> map = new TreeMap<>(results);
             List<String> labels = new ArrayList<>();
-            //System.out.println("After Sorting:");
             Set set2 = map.entrySet();
             Iterator iterator2 = set2.iterator();
             int counter = 0;
             while (iterator2.hasNext()) {
                 Map.Entry me2 = (Map.Entry) iterator2.next();
-                //System.out.print(me2.getKey() + ": ");
-                //System.out.println(me2.getValue());
                 labels.add((String) me2.getValue());
                 counter++;
                 if (counter == k)
@@ -62,78 +58,95 @@ public class Knn {
             Set<String> unique = new HashSet<String>(labels);
             HashMap<Integer, String> frequency = new HashMap<>();
             for (String key : unique) {
-                frequency.put(Collections.frequency(labels,key), key);
-                //System.out.println(key + " : " + Collections.frequency(labels,key) );
+                frequency.put(Collections.frequency(labels, key), key);
             }
             NavigableMap<Integer, String> frequencyResult = new TreeMap<>(frequency);
-            if(classifyArt.getLabel().equals(frequencyResult.lastEntry().getValue())) {
+            if (classifyArt.getLabel().equals(frequencyResult.lastEntry().getValue())) {
                 setPercentageFound(classifyArt.getLabel());
-            }
-            else
+            } else
                 setPercentageNotFound(classifyArt.getLabel());
         }
         System.out.println("=======================================\n RESULTS: ");
         showPercentage();
     }
 
-    private double euclideanMetric(int [][] values){
-        double result =0;
-        double [] tempResults = new double[values.length];
-        for(int i=0; i<values.length; i++){
-            tempResults[i] = Math.pow((values[i][0] - values[i][1]), 2);
-        }
-        for(int j=0; j<tempResults.length;j++){
-            result += tempResults[j];
+    private double euclideanMetric(int[][] values) {
+        double result = 0;
+        for (int i = 0; i < values.length; i++) {
+            result += Math.pow((values[i][0] - values[i][1]), 2);
         }
         return Math.abs(result);
     }
 
-    private void setPercentageFound(String label){
+    private double manhattanMetric(int[][] values) {
+        double result = 0;
+        for (int i = 0; i < values.length; i++) {
+            result += Math.abs(values[i][0] - values[i][1]);
+        }
+        return result;
+    }
+
+    private double chebyshevMetric(int[][] values){
+        List<Integer> tempResults = new ArrayList<>();
+        for (int i=0; i<values.length; i++){
+            tempResults.add(Math.abs(values[i][0] - values[i][1]));
+        }
+        return Collections.max(tempResults);
+    }
+
+
+    // METHODS FOR SHOWING RESULTS AND SAVING DETAILED RESULTS TO FILE
+    private void setPercentageFound(String label) {
 
         int previous = percentage.get(label)[0];
         int total = percentage.get(label)[1];
-        percentage.put(label, new int[]{previous+1,total+1});
-    }
-    private void setPercentageNotFound(String label){
-        int previous = percentage.get(label)[0];
-        int total = percentage.get(label)[1];
-        percentage.put(label, new int[]{previous,total+1});
+        percentage.put(label, new int[]{previous + 1, total + 1});
     }
 
-    private void showPercentage(){
-        double succeded = 0; double all = 0;
+    private void setPercentageNotFound(String label) {
+        int previous = percentage.get(label)[0];
+        int total = percentage.get(label)[1];
+        percentage.put(label, new int[]{previous, total + 1});
+    }
+
+    private void showPercentage() {
+        double succeded = 0;
+        double all = 0;
         Set set = this.percentage.entrySet();
         Iterator iterator = set.iterator();
         List<String> detailedAnswers = new ArrayList<>();
-        while(iterator.hasNext()){
+        while (iterator.hasNext()) {
             Map.Entry mentry = (Map.Entry) iterator.next();
             succeded += percentage.get(mentry.getKey())[0];
             all += percentage.get(mentry.getKey())[1];
-            String v1 = String.format("%1$-10s %2$10d", mentry.getKey(),  percentage.get(mentry.getKey())[1]);
+            String v1 = String.format("%1$-10s %2$10d", mentry.getKey(), percentage.get(mentry.getKey())[1]);
             String v2 = String.format("%1$-10s %2$10d", "Success", percentage.get(mentry.getKey())[0]);
             String v3 = String.format("%1$-10s %2$10d", "Fail", (percentage.get(mentry.getKey())[1] - percentage.get(mentry.getKey())[0]));
-            String v4 = String.format("%1$-10s %2$10d", "RESULT", percentage.get(mentry.getKey())[0]*100/percentage.get(mentry.getKey())[1]);
-            v4 +="%";
-            detailedAnswers.add(v1);detailedAnswers.add(v2);detailedAnswers.add(v3);detailedAnswers.add(v4+"\n");
+            String v4 = String.format("%1$-10s %2$10d", "RESULT", percentage.get(mentry.getKey())[0] * 100 / percentage.get(mentry.getKey())[1]);
+            v4 += "%";
+            detailedAnswers.add(v1);
+            detailedAnswers.add(v2);
+            detailedAnswers.add(v3);
+            detailedAnswers.add(v4 + "\n");
         }
-        System.out.println("\nClassification succeded in "+ succeded*100/all+"%");
+        System.out.println("\nClassification succeded in " + succeded * 100 / all + "%");
         saveDetailedResultToFile(detailedAnswers);
     }
 
-    private void initializePercentage(){
-        for(int i=0; i<places.length;i++){
-            percentage.put(places[i], new int[]{0,0});
+    private void initializePercentage() {
+        for (int i = 0; i < places.length; i++) {
+            percentage.put(places[i], new int[]{0, 0});
         }
     }
 
-    private void saveDetailedResultToFile(List<String> results){
+    private void saveDetailedResultToFile(List<String> results) {
         try {
             PrintWriter out = new PrintWriter("detailedResult.txt");
-            for (String s :results) {
+            for (String s : results) {
                 out.println(s);
             }
             out.close();
-            System.out.println("Detailed results saved to -> "+ "detailedResult.txt");
+            System.out.println("Detailed results saved to -> " + "detailedResult.txt");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
